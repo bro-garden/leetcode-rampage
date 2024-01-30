c93e70e6b25a3d55ba4bf91be1ff1fa9d3eb8f93
Sebastian Gomez <gomeztabaresjuansebastian@gmail.com>
2024-01-30 09:10:21 -0500

    day2 challenge solve by sgt

---
+++ @juanse1904/day2.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+ var merge = function(nums1, m, nums2, n) {
+    const auxArr = [...nums1];
+   
+   nums2.forEach((num,i)=>{
+       nums1.splice(m+i,0,num)
+   });
+
+   nums1.length= n + m;
+    nums1.sort((j,k)=>{
+        return j - k;
+    });
+};
